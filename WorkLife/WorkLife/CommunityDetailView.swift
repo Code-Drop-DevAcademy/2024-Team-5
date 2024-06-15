7f8c102190907d8c9dc2e3474c37aa461b6625db
TakeMos <kangh0423@gmail.com>
2024-06-15 11:17:58 +0900

    [FEAT/#13] 상세 뷰 그리기

---
+++ WorkLife/WorkLife/CommunityDetailView.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+struct CommunityDetailView: View {
+    @State private var date: String = ""
+    @State private var weather: String = ""
+    @State private var diaryPic: Image
+    @State private var diaryText: String = ""
+    
+    var body: some View {
+        
+    }
+}
